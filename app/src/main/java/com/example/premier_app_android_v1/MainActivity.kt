0d74ab0a9be96c96fdf4c25be978dd28a60790a4
Dev Zakaria <Azizi.zakaria.solicode@gmail.com>
2024-10-09 04:08:48 +0100

    Update

--- app/src/main/java/com/example/premier_app_android_v1/MainActivity.kt
+++ app/src/main/java/com/example/premier_app_android_v1/MainActivity.kt
@@ -3,39 +3,25 @@ package com.example.premier_app_android_v1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.premier_app_android_v1.ui.theme.Premier_App_Android_v1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             Premier_App_Android_v1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Zakaria",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Greeting(name = "Zakaria")
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(name: String) {
+    Text(text = "Hello, $name! Welcome to My first Android app!")
 }
 
 @Preview(showBackground = true)
