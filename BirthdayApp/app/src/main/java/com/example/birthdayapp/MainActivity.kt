636b211ac88837e16dd3396819c2121b48b75dcc
Dev Zakaria <Azizi.zakaria.solicode@gmail.com>
2024-10-09 22:36:45 +0100

    Add Fun Birthday Card Preview

--- BirthdayApp/app/src/main/java/com/example/birthdayapp/MainActivity.kt
+++ BirthdayApp/app/src/main/java/com/example/birthdayapp/MainActivity.kt
@@ -40,8 +40,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    BirthdayAppTheme {
-        Greeting("Android")
+fun BirthdayCardPreview() {
+    HappyBirthdayTheme {
+        Greeting("Zakaria")
     }
 }
